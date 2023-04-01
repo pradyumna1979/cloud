bf2a50e41e1aff7ee8c298c6dbcb6be1e2d6c3a6
pradyumna1979 <kuni.khadanga@gmail.com>
2023-04-01 23:46:17 +0530

    Docker File with test

---
+++ src/main/java/com/pradyumna/SpringbootDockerJenkinsCloudApplication.java
@@ -0,0 +1,13 @@
+package com.pradyumna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerJenkinsCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerJenkinsCloudApplication.class, args);
+	}
+
+}
